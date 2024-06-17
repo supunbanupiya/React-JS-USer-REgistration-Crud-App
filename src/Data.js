2613f6acc69a79190809f329c823cfe42205f737
Supunbanupiya <supun@petpos.com>
2024-06-17 11:25:59 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,20 @@
+export const userList = [
+    {
+        id:1,
+        name:"sumudu",
+        email:"sumudu@gmail.com",
+        
+    },
+    {
+        id:2,
+        name:"jeewan",
+        email:"jeewan@gmail.com",
+        
+    },
+    {
+        id:3,
+        name:"maduri",
+        email:"maduri@gmail.com",
+        
+    },
+]
---
+++ src/Update.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { updateUser } from './UserReducer';
+
+const Update = () => {
+   const {id} = useParams();
+
+   const users = useSelector((state=>state.users));
+   const existingUser = users.filter(f=>f.id===id);
+   const {name,email} = existingUser[0];
+   const[uname, setName]= useState(name)
+   const[uemail, setEmail]= useState(email)
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   
+   const handleUpdate =(e)=>{
+     e.preventDefault();
+     dispatch(updateUser({
+        id:id,
+        name:uname,
+        email:uemail
+     }))
+     navigate('/')
+   }
+    return (
+ <div className='d-flex w-100 vh-100 justify-content-center align-items-center'>
+      <div className='w-50 border bg-secondary text-white p-5'>
+        <h3>Update User</h3>
+        <form onSubmit={handleUpdate}>
+          <div>
+            <label htmlFor='name'>Name:</label>
+            <input type='text' name="name" className='form-control' placeholder='update name'
+           value={uname} onChange={e=>setName(e.target.value)}/>
+          </div>
+          <div>
+            <label htmlFor='email'>Email:</label>
+            <input type='text' name="email" className='form-control'placeholder='update email'
+           value={uemail} onChange={e=>setEmail(e.target.value)}/>
+          </div><br/>
+           <button className='btn btn-info'>Update</button>
+        </form>
+      </div>
+ </div>
+  
+  )
+}
+
+export default Update
